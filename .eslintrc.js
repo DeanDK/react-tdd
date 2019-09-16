7af1d27e8bbfb9b2f820a2dd7b296e449699738e
DeanDK <deanbozic12@gmail.com>
2019-09-17 00:20:21 +0200

    added eslint conf

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        'codingitwrong',
+    ],
+    parser: 'babel-eslint',
+    plugins: [
+        'jest',
+        'react'
+    ],
+    env: {
+        'browser': true,
+        'jest/globals': true,
+    },
+};
